74587694daaa5b124cc947959c4a608d1c87afd9
houyutao <703142996@qq.com>
2016-06-26 22:35:49 +0800

    ori

---
+++ src/org/javachina/servlet/LoginPageServlet.java
@@ -0,0 +1,47 @@
+package org.javachina.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginPageServlet
+ */
+@WebServlet("/LoginPageServlet")
+public class LoginPageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginPageServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+    	PrintWriter writer = response.getWriter();
+    	writer.println("<html>");
+		writer.println("<body bgColor=yellow><font color='red'>天王盖地虎~</font><body>");
+		writer.println("</html>");
+		System.out.println("LoginServlet的get方法被调用了");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("LoginServlet的post方法被调用了");
+	}
+
+}
